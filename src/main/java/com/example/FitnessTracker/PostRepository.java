6a2def8916929824b3f4290c5c2a83bc238e1d11
DavidLengel <david@Davids-MacBook-Pro.local>
2020-02-28 15:54:41 -0500

    Code refactor for IndividualFeed queries.

--- src/main/java/com/example/FitnessTracker/PostRepository.java
+++ src/main/java/com/example/FitnessTracker/PostRepository.java
@@ -2,7 +2,10 @@ package com.example.FitnessTracker;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface PostRepository extends CrudRepository<Post, Integer> {
 
     Post findByActivityID(int activityid);
+    ArrayList<Post> findAllByUserID(int userid);
 }
