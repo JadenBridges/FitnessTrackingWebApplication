697017bc26878087f417fae27023b74e57672711
Chris <cmd6113@psu.edu>
2020-04-22 13:56:43 -0400

    Overwrite WebConfig

--- src/main/java/com/example/FitnessTracker/controllers/FitnessTrackerController.java
+++ src/main/java/com/example/FitnessTracker/controllers/FitnessTrackerController.java
@@ -3,6 +3,7 @@ package com.example.FitnessTracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +14,14 @@ public class FitnessTrackerController {
     public FitnessTrackerController(){}
 
     @GetMapping("/feeds")
-    public String feeds(Model m) {
-        Map<String, String> map = new HashMap<>();
-        String t = "Spring <b>Boot</b>";
-        map.put("spring", t);
-        m.mergeAttributes(map);
+    public String displayFeeds() {
         return "feeds";
     }
+
+    @RequestMapping("/")
+    public String displayLogin() {
+        return "UserForm";
+    }
+
+
 }
